f778f10dfea05df639ac251cacc2c196b19846f8
lmdxnmx <nikita021103@mail.ru>
2024-05-30 09:53:17 +0300

    fix

---
+++ Sources/Decoder/include/Decoder.h
@@ -0,0 +1,15 @@
+//
+//  Header.h
+//  
+//
+//  Created by Никита on 30.05.2024.
+//
+
+#ifndef Decoder_h
+#define Decoder_h
+#import "lame.h"
+#import "LKCDecodeHeart.h"
+#import "LKCPlayManager.h"
+#import "LMTPDecoder.h"
+
+#endif /* Header_h */
