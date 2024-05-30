28ed69ee7c7851c5630c745bc2aa421e4d727a37
lmdxnmx <nikita021103@mail.ru>
2024-05-30 14:13:25 +0300

    fix

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
             ],
             linkerSettings: [
                 .linkedLibrary("c++"),
-                .unsafeFlags(["-L", "Sources/Decoder", "-lLMTPDecoder", "-llame"])
+                .unsafeFlags(["-L", "Sources/Decoder", "-lLMTPDecoder"])
             ]
         ),
         .target(
