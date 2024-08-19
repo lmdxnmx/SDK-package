06ede6a37273f8e6aabe74be0ac5c5a7b6c4fa4a
lmdxnmx <nikita021103@mail.ru>
2024-08-19 11:02:26 +0300

    fix

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
             ],
             linkerSettings: [
                 .linkedLibrary("c++"),
-          
+                .linkedLibrary("libLMTPDecoder"),
             ]
         ),
         .target(
