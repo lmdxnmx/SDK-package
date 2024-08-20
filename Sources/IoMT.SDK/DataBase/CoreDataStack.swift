a06182f8618a3f65d95cede324962ee831538cbf
lmdxnmx <nikita021103@mail.ru>
2024-08-20 11:57:26 +0300

    fix

--- Sources/IoMT.SDK/DataBase/CoreDataStack.swift
+++ Sources/IoMT.SDK/DataBase/CoreDataStack.swift
@@ -1,45 +1,35 @@
-//
-//  CoreDataStack.swift
-//  MedicalApp
-//
-//  Created by Никита on 08.02.2024.
-//
 import CoreData
 import Foundation
-class PersistentContainer: NSPersistentContainer { }
+
 class CoreDataStack {
     
     // Создание shared экземпляра для использования во всем приложении
     static let shared = CoreDataStack()
-   
-    // Создание подкласса NSPersistentContainer
-    open class PersistentContainer: NSPersistentContainer { }
-
-    lazy public var persistentContainer: PersistentContainer! = {
+    
+    // Инициализация контейнера с ленивой загрузкой
+    lazy var persistentContainer: NSPersistentContainer = {
         // Попытка получить URL модели данных в пакете
         guard let modelURL = Bundle.module.url(forResource: "Observation-2", withExtension: "momd") else {
-            return nil
+            fatalError("Не удалось найти модель данных в пакете.")
         }
         
         // Загрузка модели данных из указанного URL
         guard let model = NSManagedObjectModel(contentsOf: modelURL) else {
-            return nil
+            fatalError("Не удалось создать модель данных из URL: \(modelURL).")
         }
         
         // Инициализация контейнера с именем и моделью данных
-        let container = PersistentContainer(name: "Observation-2", managedObjectModel: model)
+        let container = NSPersistentContainer(name: "Observation-2", managedObjectModel: model)
         
         // Загрузка персистентного хранилища
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+        container.loadPersistentStores { storeDescription, error in
             if let error = error as NSError? {
-                print("Ошибка при загрузке персистентного хранилища: \(error), \(error.userInfo)")
+                fatalError("Ошибка при загрузке персистентного хранилища: \(error), \(error.userInfo)")
             }
-        })
+        }
         
         return container
     }()
-
-
     
     // Контекст для работы с данными в основной очереди
     var viewContext: NSManagedObjectContext {
@@ -47,14 +37,12 @@ class CoreDataStack {
     }
     
     // Сохранение изменений в контексте
-    func saveContext () {
-        let context = persistentContainer.viewContext
+    func saveContext() {
+        let context = viewContext
         if context.hasChanges {
             do {
-                // Сохранение контекста
                 try context.save()
             } catch {
-                // Обработка ошибок сохранения контекста
                 let nserror = error as NSError
                 fatalError("Не удалось сохранить контекст: \(nserror), \(nserror.userInfo)")
             }
